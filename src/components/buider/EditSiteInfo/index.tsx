182773d25cc1903880c1444cb92df1ae93d23d60
boyon99 <bo_yon_99@naver.com>
2023-06-07 17:11:49 +0900

    feat: 사이트 정보 수정 컴포넌트 레이아웃 구성

---
+++ src/components/buider/EditSiteInfo/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface EditSiteInfoProps {
+  setIsTopbarBtn: React.Dispatch<
+    React.SetStateAction<{
+      mainColorBtn: boolean;
+      editSiteInfoBtn: boolean;
+    }>
+  >;
+}
+
+export default function EditSiteInfo({ setIsTopbarBtn }: EditSiteInfoProps) {
+  return (
+    <div className="w-[400px] h-[800px] border rounded-[5px] mt-[20px] overflow-y-auto relative">
+      <button
+        className="w-[auto] fixed right-[5px] border absolute"
+        onClick={() => {
+          setIsTopbarBtn((prevState) => ({
+            mainColorBtn: false,
+            editSiteInfoBtn: !prevState.editSiteInfoBtn,
+          }));
+        }}
+      >
+        X
+      </button>
+      <div className="w-[350px] ml-[25px] h-[50px] border">
+        사이트 정보 수정
+      </div>
+      <div className="w-[350px] ml-[25px] h-[50px] border">웹 페이지 관리</div>
+      <div className="w-[350px] ml-[25px] h-[300px] border">파비콘 설정</div>
+      <div className="w-[350px] ml-[25px] h-[100px] border">
+        웹 페이지 이름 설정
+      </div>
+      <div className="w-[350px] ml-[25px] h-[100px] border">도메인 설정</div>
+      <div className="w-[350px] ml-[25px] h-[50px] border">검색 결과 관리</div>
+      <div className="w-[350px] ml-[25px] h-[100px] border">사이트 제목</div>
+      <div className="w-[350px] ml-[25px] h-[100px] border">사이트 설명</div>
+      <div className="w-[350px] ml-[25px] h-[50px] border">GA 연동</div>
+      <div className="w-[350px] ml-[25px] h-[100px] border">
+        GA 추척 ID 입력
+      </div>
+    </div>
+  );
+}
