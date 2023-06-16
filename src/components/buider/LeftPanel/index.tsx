386435d22843fe9a921ceca80bad902b740fc5a7
boyon99 <bo_yon_99@naver.com>
2023-06-16 16:13:01 +0900

    feat: Menu 컴포넌트 적용하기

--- src/components/buider/LeftPanel/index.tsx
+++ src/components/buider/LeftPanel/index.tsx
@@ -1,31 +1,37 @@
 import useStore from "@/store";
 import React, { useState } from "react";
 import { List, arrayMove } from "react-movable";
-import Menu from "../Menu";
-
-const widgets = ["헤더/푸터"];
+import { MenuA, MenuB } from "../Menu";
+import { ToggleNotText, ToggleText } from "@/components/common/toggle";
 
 export default function LeftPanal() {
-  const { widgets, addWidget, removeWidget, setWidget } = useStore();
+  const { widgets, setWidget } = useStore();
 
   return (
     <>
       {/* 테마 변경, 회사 정보 수정, 사이트 정보 수정 버튼*/}
       <div className="w-[100%] h-[185px]">
         <div className="w-[264px] h-[161px] mt-[24px] ml-[28px] border-b-[1px] border-GrayScalePrimary-400">
-          <Menu routerName="/builder/changeTheme" buttonName={"테마 변경"} />
-          <Menu
+          <MenuA routerName="/builder/changeTheme" buttonName={"테마 변경"} />
+          <MenuA
             routerName="/builder/changeCompany"
             buttonName={"회사 정보 수정"}
           />
-          <Menu
+          <MenuA
             routerName="/builder/changeSite"
             buttonName={"사이트 정보 수정"}
           />
         </div>
       </div>
       {/* 위젯 추가 삭제 및 순서 변경 */}
-      <div className="w-[100%] h-[calc(100%-185px)] overflow-y-scroll">
+      <div className="w-[100%] h-[calc(100%-185px)] overflow-y-scroll pl-[24px]">
+        {/* 토글 버튼 */}
+        <div className="ml-[130px] mb-[10px]">
+          <ToggleText toggleText="순서 변경"></ToggleText>
+        </div>
+        {/* 위젯 순서 변경 */}
+        <MenuB buttonName={"헤더/푸터"} routerName="" isActivate={false} />
+        {/* react-movable 라이브러리 사용 */}
         <List
           values={widgets}
           onChange={({ oldIndex, newIndex }) =>
@@ -46,11 +52,19 @@ export default function LeftPanal() {
                 ...props.style,
                 listStyleType: "none",
                 cursor: isDragged ? "grabbing" : "grab",
-                backgroundColor: isDragged || isSelected ? "#c2b6f0" : "#FFF",
+                border: isDragged ? "2px solid #4B48DF" : "none",
               }}
-              className="border w-[200px] ml-[30px] mt-[10px]"
+              className={
+                value.name === "헤더/푸터" ? "" : "w-[266px] h-[49px] mb-[6px]"
+              }
             >
-              {value}
+              {value.name === "헤더/푸터" ? null : (
+                <MenuB
+                  buttonName={value.name}
+                  routerName=""
+                  isActivate={true}
+                />
+              )}
             </li>
           )}
         />
