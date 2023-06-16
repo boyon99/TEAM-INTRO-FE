78e2c346077c9acd5ea6502254d566ff6dace497
boyon99 <bo_yon_99@naver.com>
2023-06-16 13:57:39 +0900

    chore: 빌더 컴포넌트 이름 변경

--- src/components/buider/LeftPanel/index.tsx
+++ src/components/buider/LeftPanel/index.tsx
@@ -1,23 +1,30 @@
 import useStore from "@/store";
-import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { List, arrayMove } from "react-movable";
+import Menu from "../Menu";
 
-export default function EditBuilder() {
+const widgets = ["헤더/푸터"];
+
+export default function LeftPanal() {
   const { widgets, addWidget, removeWidget, setWidget } = useStore();
-  const router = useRouter();
 
   return (
     <>
-      {/* 테마 변경, 회사 정보 수정, 사이트 정보 수정 */}
+      {/* 테마 변경, 회사 정보 수정, 사이트 정보 수정 버튼*/}
       <div className="w-[100%] h-[185px]">
         <div className="w-[264px] h-[161px] mt-[24px] ml-[28px] border-b-[1px] border-GrayScalePrimary-400">
-          <button onClick={() => router.push("/builder/changeTheme")}>
-            테마 변경
-          </button>
+          <Menu routerName="/builder/changeTheme" buttonName={"테마 변경"} />
+          <Menu
+            routerName="/builder/changeCompany"
+            buttonName={"회사 정보 수정"}
+          />
+          <Menu
+            routerName="/builder/changeSite"
+            buttonName={"사이트 정보 수정"}
+          />
         </div>
       </div>
-      {/* 위젯 제거 및 순서 변경 */}
+      {/* 위젯 추가 삭제 및 순서 변경 */}
       <div className="w-[100%] h-[calc(100%-185px)] overflow-y-scroll">
         <List
           values={widgets}
