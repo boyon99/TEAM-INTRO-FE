e7a8adc6e4a8ede5bf09c3dc532450cc2ae6c04a
boyon99 <bo_yon_99@naver.com>
2023-06-16 21:59:00 +0900

    feat: keyvisual 페이지 생성

---
+++ src/pages/builder/keyvisual.tsx
@@ -0,0 +1,11 @@
+import EditBuilder from "@/components/buider/LeftPanel";
+import Layout from "@/components/buider/Layout";
+import React, { useState } from "react";
+
+export default function KeyVisual() {
+  return (
+    <div>
+      <Layout>{<h1>KeyVisual</h1>}</Layout>
+    </div>
+  );
+}
