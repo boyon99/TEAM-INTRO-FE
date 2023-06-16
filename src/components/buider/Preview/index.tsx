4c8a8afc2fb2012342129c00cce531144b0d88df
boyon99 <bo_yon_99@naver.com>
2023-06-16 16:13:49 +0900

    feat: store에 저장된 widget 목록 가져오기

--- src/components/buider/Preview/index.tsx
+++ src/components/buider/Preview/index.tsx
@@ -3,16 +3,28 @@ import { Timeline, Cta, Fitness } from "@/components/buider/Widget";
 import useStore from "@/store";
 
 export default function Preview() {
-  const { widgets, addWidget, removeWidget, setWidget } = useStore();
+  const { widgets, setWidget } = useStore();
 
   return (
     <div className="border w-[945px] h-[calc(100vh-72px)]">
       {widgets.map((widget, index) => {
         return (
           <div key={index} className="border w-[100%] h-[50px] mb-[10px]">
-            {widget === "Timeline" && <Timeline />}
-            {widget === "Cta" && <Cta />}
-            {widget === "Fitness" && <Fitness />}
+            {widget.name === "헤더/푸터" && <Timeline />}
+            {widget.name === "키비주얼/슬로건" && <Cta />}
+            {widget.name === "미션/비젼" && <Fitness />}
+            {/* {widget.name === "제품/서비스 소개" && <Fitness />}
+            {widget.name === "팀 멤버" && <Fitness />}
+            {widget.name === "컨택어스" && <Fitness />}
+            {widget.name === "보도자료" && <Fitness />}
+            {widget.name === "다운로드" && <Fitness />}
+            {widget.name === "연혁" && <Fitness />}
+            {widget.name === "팀 문화" && <Fitness />}
+            {widget.name === "핵심 성과" && <Fitness />}
+            {widget.name === "파트너스" && <Fitness />}
+            {widget.name === "고객리뷰" && <Fitness />}
+            {widget.name === "채널" && <Fitness />}
+            {widget.name === "특허/인증" && <Fitness />} */}
           </div>
         );
       })}
