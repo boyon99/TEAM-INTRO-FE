638524336724eb953a0ad004b7ef24866074b600
boyon99 <bo_yon_99@naver.com>
2023-06-16 16:25:36 +0900

    feat: 주석 추가

--- src/interfaces/builder.ts
+++ src/interfaces/builder.ts
@@ -1,5 +1,5 @@
 export interface MenuProps {
-  routerName: string;
-  buttonName: string;
-  isActivate?: boolean;
+  routerName: string; // 이동할 페이지 경로
+  buttonName: string; // 버튼 이름
+  isActivate?: boolean; //위젯 추가, 삭제가 가능한 항목의 경우 true, 아닌 경우 false
 }
