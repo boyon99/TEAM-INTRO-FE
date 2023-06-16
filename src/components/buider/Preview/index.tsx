84ac8846954ef12a12e7a5f2b3fc5545560788bb
boyon99 <bo_yon_99@naver.com>
2023-06-16 21:34:25 +0900

    feat: 빌더랑 푸터 컴포넌트 분리하기

--- src/components/buider/Preview/index.tsx
+++ src/components/buider/Preview/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
-  HeaderFooter,
+  Header,
+  Footer,
   KeyVisual,
   MissionVision,
 } from "@/components/buider/Widget";
@@ -9,16 +10,15 @@ import { useRouter } from "next/router";
 
 export default function Preview() {
   // 위젯 목록 가져오기
-  const { widgets } = useStore();
+  const { widgets, theme } = useStore();
 
   return (
     <div className="border w-[945px] h-[calc(100vh-72px)]">
+      <Header theme={theme.theme} />
+
       {widgets.map((widget, index) => {
         return (
           <div key={index}>
-            {widget.routerName === "builder/headerfooter" && widget.toggle ? (
-              <HeaderFooter />
-            ) : null}
             {widget.routerName === "builder/keyvisual" && widget.toggle ? (
               <KeyVisual />
             ) : null}
@@ -28,6 +28,7 @@ export default function Preview() {
           </div>
         );
       })}
+      <Footer theme={theme.theme} />
     </div>
   );
 }
--- src/components/buider/Widget/index.tsx
+++ src/components/buider/Widget/index.tsx
@@ -1,13 +1,26 @@
+import { HeaderProps } from "@/interfaces/widget";
 import React from "react";
 
 export function KeyVisual() {
-  return <div className="">KeyVisual</div>;
+  return <div className="h-[200px] border">KeyVisual</div>;
 }
 
 export function MissionVision() {
-  return <div className="">MissionVision</div>;
+  return <div className="h-[200px] border">MissionVision</div>;
 }
 
-export function HeaderFooter() {
-  return <div className="">HeaderFooter</div>;
+export function Header({ theme }: HeaderProps) {
+  if (theme === "A") {
+    return <div className="h-[200px]">Header A</div>;
+  } else {
+    return <div className="h-[200px]">Header B</div>;
+  }
+}
+
+export function Footer({ theme }: HeaderProps) {
+  if (theme === "A") {
+    return <div className="h-[200px]">Footer A</div>;
+  } else {
+    return <div className="h-[200px]">Footer B</div>;
+  }
 }
