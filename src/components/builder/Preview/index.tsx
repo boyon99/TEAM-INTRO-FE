56a372a4bb2f2c28f808f4edcea66b179e03049e
boyon99 <bo_yon_99@naver.com>
2023-06-19 15:44:51 +0900

    fix: 스크롤 추가

--- src/components/builder/Preview/index.tsx
+++ src/components/builder/Preview/index.tsx
@@ -1,27 +1,26 @@
-import React from "react";
-import {
-  Header,
-  Footer,
-  KeyVisual,
-  MissionVision,
-} from "@/components/builder/Widget";
-import useStore from "@/store";
+import React from 'react';
+import { Header, Footer, KeyVisual, MissionVision } from '@/components/builder/Widget';
+import useStore from '@/store';
 
 export default function Preview() {
   // 위젯 목록 가져오기
   const { widgets, theme } = useStore();
 
   return (
-    <div className="border w-[945px] h-[calc(100vh-72px)]">
+    <div className="border w-[945px] h-[calc(100vh-72px)] overflow-y-scroll">
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
+      <Header theme={theme.theme} />
       <Header theme={theme.theme} />
       {widgets.map((widget, index) => {
         return (
           <div key={index}>
-            {widget.routerName === "builder/keyvisual#w-02" && widget.toggle ? (
-              <KeyVisual theme={theme.theme} />
-            ) : null}
-            {widget.routerName === "builder/missionvision#w-03" &&
-            widget.toggle ? (
+            {widget.routerName === 'builder/keyvisual#w-02' && widget.toggle ? <KeyVisual theme={theme.theme} /> : null}
+            {widget.routerName === 'builder/missionvision#w-03' && widget.toggle ? (
               <MissionVision theme={theme.theme} />
             ) : null}
           </div>
