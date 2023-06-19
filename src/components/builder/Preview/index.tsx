4a2dfa0daee34666427670606eccedcbbf6f001b
boyon99 <bo_yon_99@naver.com>
2023-06-19 15:55:02 +0900

    feat: 미리보기 위젯 컴포넌트 추가

--- src/components/builder/Preview/index.tsx
+++ src/components/builder/Preview/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
-import { Header, Footer, KeyVisual, MissionVision } from '@/components/builder/Widget';
+import {
+  Header,
+  Footer,
+  KeyVisual,
+  MissionVision,
+  ProductService,
+  TeamMember,
+  ContactUs,
+  Press,
+  Download,
+  History,
+  TeamCulture,
+  Result,
+  Partners,
+  Review,
+  Channel,
+  Patent,
+} from '@/components/builder/Widget';
 import useStore from '@/store';
 
 export default function Preview() {
@@ -9,23 +26,31 @@ export default function Preview() {
   return (
     <div className="border w-[945px] h-[calc(100vh-72px)] overflow-y-scroll">
       <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      <Header theme={theme.theme} />
-      {widgets.map((widget, index) => {
-        return (
-          <div key={index}>
-            {widget.routerName === 'builder/keyvisual#w-02' && widget.toggle ? <KeyVisual theme={theme.theme} /> : null}
-            {widget.routerName === 'builder/missionvision#w-03' && widget.toggle ? (
-              <MissionVision theme={theme.theme} />
-            ) : null}
-          </div>
-        );
-      })}
+      {widgets.map((widget, index) => (
+        <div key={index}>
+          {widget.routerName === 'builder/keyvisual#w-01' && widget.toggle ? <KeyVisual theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/missionvision#w-02' && widget.toggle ? (
+            <MissionVision theme={theme.theme} />
+          ) : null}
+
+          {widget.routerName === 'builder/productservice#w-03' && widget.toggle ? (
+            <ProductService theme={theme.theme} />
+          ) : null}
+          {widget.routerName === 'builder/teammember#w-04' && widget.toggle ? <TeamMember theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/contactu#w-05' && widget.toggle ? <ContactUs theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/press#w-06' && widget.toggle ? <Press theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/download#w-07' && widget.toggle ? <Download theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/history#w-08' && widget.toggle ? <History theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/teamculture#w-09' && widget.toggle ? (
+            <TeamCulture theme={theme.theme} />
+          ) : null}
+          {widget.routerName === 'builder/result#w-10' && widget.toggle ? <Result theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/partners#w-11' && widget.toggle ? <Partners theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/review#w-12' && widget.toggle ? <Review theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/channel#w-13' && widget.toggle ? <Channel theme={theme.theme} /> : null}
+          {widget.routerName === 'builder/patent#w-14' && widget.toggle ? <Patent theme={theme.theme} /> : null}
+        </div>
+      ))}
       <Footer theme={theme.theme} />
     </div>
   );
