2d84c3f09ed1c522df8f59f9ce4ca3f5670eec0e
Valentin1495 <newface95@icloud.com>
2023-06-24 17:35:06 +0900

    style: table 스타일 수정

---
+++ src/components/dashboard/downloads/DownloadCount.tsx
@@ -0,0 +1,8 @@
+export default function DownloadCount() {
+  return (
+    <div className="bg-GrayScalePrimary-150 w-[230px] h-[133px]">
+      <span>오늘</span>
+      <div>asdf</div>
+    </div>
+  );
+}
---
+++ src/components/dashboard/downloads/DownloadCountChart.tsx
@@ -0,0 +1,72 @@
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import CustomTooltip from '../CustomTooltip';
+
+export default function DownloadCountChart() {
+  const data = [
+    {
+      name: '06.06',
+      미디어킷: 10,
+      회사소개서: 30,
+    },
+    {
+      name: '06.07',
+      미디어킷: 30,
+      회사소개서: 40,
+    },
+    {
+      name: '06.08',
+      미디어킷: 20,
+      회사소개서: 20,
+    },
+    {
+      name: '06.09',
+      미디어킷: 40,
+      회사소개서: 30,
+    },
+    {
+      name: '06.10',
+      미디어킷: 30,
+      회사소개서: 10,
+    },
+    {
+      name: '06.11',
+      미디어킷: 10,
+      회사소개서: 20,
+    },
+    {
+      name: '06.12',
+      미디어킷: 20,
+      회사소개서: 10,
+    },
+  ];
+
+  return (
+    <div className="w-[608px]">
+      <ResponsiveContainer width="100%" height={148} className="mt-3">
+        <LineChart data={data}>
+          <XAxis
+            tickLine={false}
+            tick={{ fontSize: '12px', fill: '#6E6D86' }}
+            tickMargin={13}
+            axisLine={{ stroke: '#EAEAEE', strokeWidth: 1 }}
+            dataKey="name"
+            padding={{ left: 30, right: 30 }}
+          />
+          <YAxis axisLine={false} tickLine={false} tick={{ fontSize: '12px', fill: '#6E6D86' }} tickCount={5} />
+          <CartesianGrid strokeDasharray="3 3" stroke="#EAEAEE" vertical={false} />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend
+            align="right"
+            layout="vertical"
+            verticalAlign="top"
+            iconType="square"
+            iconSize={8}
+            wrapperStyle={{ fontSize: '10px' }}
+          />
+          <Line type="linear" dataKey="미디어킷" stroke="#E77373" strokeWidth={2} />
+          <Line type="linear" dataKey="회사소개서" stroke="#7673E7" strokeWidth={2} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
