7e060780b5f7b4c4850eca64b16c07e30dd1b094
Valentin1495 <newface95@icloud.com>
2023-06-24 17:59:54 +0900

    feat: 엑셀 다운로드 모달

--- src/components/dashboard/contact/ContactContents.tsx
+++ src/components/dashboard/contact/ContactContents.tsx
@@ -1,8 +1,15 @@
 import { PrimaryButton } from '../../common/button';
 import InquiryTable from './InquiryTable';
 import Title from '../Title';
+import { useState } from 'react';
+import useModal from '@/hooks/useModal';
+import { createPortal } from 'react-dom';
+import { ExcelDownloadModal } from '@/components/common/popup';
 
 export default function ContactContents() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const { openModal, closeModal } = useModal(showModal, setShowModal);
+
   return (
     <div className="border-l-2 border-l-GrayScalePrimary-200 bg-GrayScalePrimary-100 w-full px-9 pt-6 py-[39px] h-[calc(100vh-75px)]">
       <Title title="연락 관리" style="flex items-center relative">
@@ -10,8 +17,9 @@ export default function ContactContents() {
           type="primary"
           text="엑셀 다운로드"
           classname="ml-auto font-bold text-base w-32 h-10 mr-6"
-          onClick={() => {}}
+          onClick={openModal}
         />
+        {showModal && createPortal(<ExcelDownloadModal handleClick={closeModal} />, document.body)}
       </Title>
       <InquiryTable />
     </div>
--- src/components/dashboard/visitors/VisitorsTableContainer.tsx
+++ src/components/dashboard/visitors/VisitorsTableContainer.tsx
@@ -1,6 +1,10 @@
 import { PrimaryButton } from '@/components/common/button';
 import Dropdown from './Dropdown';
 import VisitorsTable from './VisitorsTable';
+import { useState } from 'react';
+import useModal from '@/hooks/useModal';
+import { createPortal } from 'react-dom';
+import { ExcelDownloadModal } from '@/components/common/popup';
 
 export default function VisitorsTableContainer() {
   const data = [
@@ -20,7 +24,8 @@ export default function VisitorsTableContainer() {
       date: '23.06.10',
     },
   ];
-
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const { openModal, closeModal } = useModal(showModal, setShowModal);
   return (
     <div className="h-[calc(100vh-224px)] border border-GrayScalePrimary-150 bg-white rounded-xl mt-5">
       {data ? (
@@ -31,8 +36,9 @@ export default function VisitorsTableContainer() {
               type="primary"
               text="엑셀 다운로드"
               classname="font-bold text-base w-32 h-10 mr-6 absolute right-0 top-1/2 -translate-y-1/2"
-              onClick={() => {}}
+              onClick={openModal}
             />
+            {showModal && createPortal(<ExcelDownloadModal handleClick={closeModal} />, document.body)}
           </section>
           <VisitorsTable data={data} />
         </div>
