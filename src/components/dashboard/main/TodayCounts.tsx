c59f0cf5ad111698d5c20531324c3e0dbbab4d08
Valentin1495 <newface95@icloud.com>
2023-06-26 18:19:38 +0900

    feat: 대시보드 > 메인 메뉴 기능 구현 (#92)
    api 이용하여 데이터를 동적으로 불러와 오늘과 주간 현황 렌더링

--- src/components/dashboard/main/TodayCounts.tsx
+++ src/components/dashboard/main/TodayCounts.tsx
@@ -1,42 +1,27 @@
 import Count from './Count';
+import { format } from 'date-fns';
 
-export default function TodayCounts() {
-  const data = [
-    {
-      id: 1,
-      title: '조회수',
-      num1: 10,
-      num2: 8,
-      date: '2023.06.10 15:52 현재',
-    },
-    {
-      id: 2,
-      title: '공유 횟수',
-      num1: 10,
-      num2: -8,
-      date: '2023.06.10 15:52 현재',
-    },
-    {
-      id: 3,
-      title: '연락 건수',
-      num1: 10,
-      num2: 8,
-      date: '2023.06.10 15:52 현재',
-    },
-    {
-      id: 4,
-      title: '다운로드 횟수',
-      num1: 10,
-      num2: -8,
-      date: '2023.06.10 15:52 현재',
-    },
-  ];
+export default function TodayCounts({ data }: { data: DashboardData }) {
+  const { view, sharing, contact_us_log, download_log } = data;
+  const date = new Date();
+  const formattedDate = format(date, 'yyyy.MM.dd HH:mm');
 
   return (
-    <div className='grid grid-cols-4 mt-5 gap-x-5'>
-      {data.map((el) => (
-        <Count key={el.id} {...el} />
-      ))}
+    <div className="grid grid-cols-4 mt-5 gap-x-5">
+      <Count today={view[6]} variation={view[6] - view[5]} title={'조회수'} date={formattedDate} />
+      <Count today={sharing[6]} variation={sharing[6] - sharing[5]} title={'공유 횟수'} date={formattedDate} />
+      <Count
+        today={contact_us_log[6]}
+        variation={contact_us_log[6] - contact_us_log[5]}
+        title={'연락 건수'}
+        date={formattedDate}
+      />
+      <Count
+        today={download_log[6]}
+        variation={download_log[6] - download_log[5]}
+        title={'다운로드 횟수'}
+        date={formattedDate}
+      />
     </div>
   );
 }
