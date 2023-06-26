ee634689303cc3546ee8df70651fa52d241e2b83
boyon99 <bo_yon_99@naver.com>
2023-06-26 22:26:32 +0900

    fix: get 요청 시 데이터 값이 없을 때 데이터를 추가하지 못하도록 변경

--- src/pages/builder/index.tsx
+++ src/pages/builder/index.tsx
@@ -32,10 +32,10 @@ export default function Builder() {
       setSiteInfo(builderData.site_info);
       setTheme({ theme_type: builderData.theme.type, color: builderData.theme.color });
       setKeyVisual({
-        background: keyvisual.background,
-        slogan: keyvisual.slogan,
-        filter: keyvisual.filter,
-        slogan_detail: keyvisual.slogan_detail,
+        background: keyvisual.background === undefined ? '' : keyvisual.background,
+        slogan: keyvisual.slogan === undefined ? '' : keyvisual.slogan,
+        filter: keyvisual.filter === undefined ? '' : keyvisual.filter,
+        slogan_detail: keyvisual.slogan_detail === undefined ? '' : keyvisual.slogan_detail,
       });
       setMissionVision({
         mission: missionvision.mission,
@@ -43,22 +43,27 @@ export default function Builder() {
         vision: missionvision.vision,
         vision_detail: missionvision.vision_detail,
       });
-      setChannel({
-        instagram_status: channel.sns_list.instagram_status,
-        instagram: channel.sns_list.instagram,
-        linked_in_status: channel.sns_list.linked_in_status,
-        linked_in: channel.sns_list.linked_in,
-        youtube_status: channel.sns_list.youtube_status,
-        youtube: channel.sns_list.youtube,
-        notion_status: channel.sns_list.notion_status,
-        notion: channel.sns_list.notion,
-        naver_blog_status: channel.sns_list.naver_blog_status,
-        naver_blog: channel.sns_list.naver_blog,
-        brunch_stroy_status: channel.sns_list.brunch_stroy_status,
-        brunch_stroy: channel.sns_list.brunch_stroy,
-        facebook_status: channel.sns_list.facebook_status,
-        facebook: channel.sns_list.facebook,
-      });
+      if (channel.sns_list === undefined) {
+      } else {
+        setChannel({
+          instagram_status: channel.sns_list.instagram_status === undefined ? false : channel.sns_list.instagram_status,
+          instagram: channel.sns_list.instagram === undefined ? '' : channel.sns_list.instagram,
+          linked_in_status: channel.sns_list.linked_in_status === undefined ? false : channel.sns_list.linked_in_status,
+          linked_in: channel.sns_list.linked_in === undefined ? '' : channel.sns_list.linked_in,
+          youtube_status: channel.sns_list.youtube_status === undefined ? false : channel.sns_list.youtube_status,
+          youtube: channel.sns_list.youtube === undefined ? '' : channel.sns_list.youtube,
+          notion_status: channel.sns_list.notion_status === undefined ? false : channel.sns_list.notion_status,
+          notion: channel.sns_list.notion === undefined ? '' : channel.sns_list.notion,
+          naver_blog_status:
+            channel.sns_list.naver_blog_status === undefined ? false : channel.sns_list.naver_blog_status,
+          naver_blog: channel.sns_list.naver_blog === undefined ? '' : channel.sns_list.naver_blog,
+          brunch_stroy_status:
+            channel.sns_list.brunch_stroy_status === undefined ? false : channel.sns_list.brunch_stroy_status,
+          brunch_stroy: channel.sns_list.brunch_stroy === undefined ? '' : channel.sns_list.brunch_stroy,
+          facebook_status: channel.sns_list.facebook_status === undefined ? false : channel.sns_list.facebook_status,
+          facebook: channel.sns_list.facebook === undefined ? '' : channel.sns_list.facebook,
+        });
+      }
     }
   }, [builderData]);
 
